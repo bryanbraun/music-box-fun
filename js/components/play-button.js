3addef4cc29045ddd8c1c881e1194b7f82c917e0
Bryan Braun <bbraun7@gmail.com>
2019-10-29 21:07:15 -0400

    feat: first pass at a button for extending song length

--- js/components/play-button.js
+++ js/components/play-button.js
@@ -41,10 +41,10 @@ export class PlayButton extends Component {
   render() {
     const isScrolling = musicBoxStore.state.appState.isScrolling;
     const classes = classNames('play-button', { 'is-playing': isScrolling });
-    const buttonText = isScrolling ? 'Pause' : 'Play';
+    const buttonText = isScrolling ? 'Pause (Space)' : 'Play (Space)';
 
     this.element.innerHTML = `
-      <button class="${classes}" aria-pressed="${isScrolling}" aria-label="${buttonText}" role="button" >
+      <button class="${classes}" aria-pressed="${isScrolling}" aria-label="${buttonText}" title="${buttonText}" role="button" >
         ${this.getIconSvg(isScrolling)}
       </button>
     `;
