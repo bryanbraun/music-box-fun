471cc6b6ecfe735a25331071b85aa6cfbb4fdf18
Bryan Braun <bbraun7@gmail.com>
2019-10-31 22:49:51 -0400

    fix: bring back the 'share this song on twitter' button
    This time, it's simpler and doesn't have a race condition! Whoohoo!

--- js/components/twitter-share-button.js
+++ js/components/twitter-share-button.js
@@ -1,21 +1,40 @@
 import { Component } from './component.js';
 import { musicBoxStore } from '../music-box-store.js';
 
+// This uses the Twitter intents API to create a customized twitter share button. The pop-up is
+// powered by the same Twitter Javascript that loads the timeline. It detects the href value of
+// my button and adds an event listener to the document.
 export class TwitterShareButton extends Component {
   constructor(props) {
     super({
-      renderTrigger: 'state',
-      element: document.querySelector('#twitter-share__updated')
+      renderTrigger: 'songState',
+      element: document.querySelector('#twitter-share')
     });
   }
 
-  // render() {
-  //   this.element.innerHTML = `
-  //     <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large"
-  //       data-text="${musicBoxStore.state.songState.songTitle || 'Untitled Song'}" data-url="${document.location.href}"
-  //       data-related="musicboxfun" data-dnt="true" data-show-count="false"></a>
-  //   `;
+  // This code was borrowed from 'Ridiculously Responsive Social Sharing Buttons' (RRSSB). It
+  // recursively decodes the string first, to ensure we aren't double encoding. See the original context:
+  // https://github.com/kni-labs/rrssb/blob/270f9273321c86c8d7193a7978727bad3d81ff15/js/rrssb.js#L106
+  encodeString(string) {
+		if (string !== undefined && string !== null) {
+			if (string.match(/%[0-9a-f]{2}/i) !== null) {
+				string = decodeURIComponent(string);
+				encodeString(string);
+			} else {
+				return encodeURIComponent(string);
+			}
+		}
+  }
 
-  //   window.twttr.widgets.load(this.element);
-  // }
+  render() {
+    const songTitle = this.encodeString(musicBoxStore.state.songState.songTitle || 'Untitled Song');
+    const songUrl = this.encodeString(document.location.href);
+
+    this.element.innerHTML = `
+      <a class="share-button" href="https://twitter.com/intent/tweet?text=${songTitle}%0A${songUrl}&related=musicboxfun">
+        <i></i>
+        Share this song
+      </a>
+    `;
+  }
 }
