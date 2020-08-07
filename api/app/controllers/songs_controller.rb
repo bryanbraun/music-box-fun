258b93a3672fb1504d760c5184453724b859b514
Bryan Braun <bbraun7@gmail.com>
2020-08-07 10:21:25 -0400

    feat: add a basic functional search endpoint

--- api/app/controllers/songs_controller.rb
+++ api/app/controllers/songs_controller.rb
@@ -6,7 +6,8 @@ class SongsController < ApplicationController
     # if it's a search query, search for the right song
     # https://stackoverflow.com/a/1081720/1154642
     if search_param
-      render json: { query: search_param }
+      result = Song.search_by_title_fulltext(search_param)
+      render json: result
     else
       # If it's not a search, return the 20 most recent songs
       render json: Song.order(created_at: :desc).limit(20).as_json(only: [:title, :data])
--- api/app/models/song.rb
+++ api/app/models/song.rb
@@ -1,2 +1,8 @@
 class Song < ApplicationRecord
+  include PgSearch::Model
+  # See: https://github.com/Casecommons/pg_search#pg_search_scope
+  pg_search_scope :search_by_title_fulltext, against: :title, using: [:tsearch]
+
+  # pg_search_scope :search_by_title_trigram, against: :title, using: [:trigram]
+
 end
