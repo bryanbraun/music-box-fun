43f4ddf66ccb9ed0509983765d890a9a3ca42b69
Bryan Braun <bbraun7@gmail.com>
2024-08-31 00:46:41 -0400

    WIP: foundations for full-workspace selection

--- site/public/js/components/selection.js
+++ site/public/js/components/selection.js
@@ -3,19 +3,12 @@ import { musicBoxStore } from '../music-box-store.js';
 import { throttle } from '../utils/throttle.js';
 import { cloneDeep } from '../utils/clone.js';
 import { getCurrentPitchArray } from '../common/box-types.js';
-import { PAPER_SIDE_MARGIN } from '../constants.js';
 import { onClickOutside } from '../common/on-click-outside.js';
-
-// We use this (instead of offsetX) because we need XPos relative to the CSS "position: relative" parent.
-function getRelativeXPos(mouseEvent) {
-  const offsetParentElPageLeft = mouseEvent.currentTarget.offsetParent.getBoundingClientRect().left;
-  return mouseEvent.pageX - offsetParentElPageLeft;
-}
 
 export class Selection extends MBComponent {
   constructor() {
     super({
-      element: document.querySelector('#selection')
+      element: document.querySelector('#equipment')
     });
 
     // drag positions relative to the offsetParent (the CSS "position: relative" parent)
@@ -25,6 +18,7 @@ export class Selection extends MBComponent {
     // cached to prevent repetitive DOM queries
     this.selectionZoneEl = null;
     this.selectionBoxEl = null;
+    this.noteLinesEl = document.querySelector('#note-lines');
 
     // Throttle the dragging event to reduce re-renders (while snapping is set to none).
     // We could throttle mousemove, but there's little benefit (mousemove alone has very
@@ -36,32 +30,32 @@ export class Selection extends MBComponent {
     this.handleMouseOut = this.handleMouseOut.bind(this);
     this.handleMouseMove = this.handleMouseMove.bind(this);
 
-    onClickOutside('.selection-drag-zone', this.deselectAllNotes);
+    onClickOutside('#equipment', this.deselectAllNotes);
   }
 
   handleDragging(event) {
     // Resize drag zone
     this.selectionZoneEl.classList.add('is-dragging');
 
-    const relativeYPos = event.offsetY;
-    const relativeXPos = getRelativeXPos(event);
-    const isYInverted = relativeYPos < this.dragStartYPos;
-    const isXInverted = relativeXPos < this.dragStartXPos;
+    const isYInverted = event.pageY < this.dragStartYPos;
+    const isXInverted = event.pageX < this.dragStartXPos;
 
     // Draw selection box in CSS
     this.selectionBoxEl.style = `
-      top: ${isYInverted ? relativeYPos : this.dragStartYPos}px;
-      left: ${isXInverted ? relativeXPos : this.dragStartXPos}px;
-      width: ${Math.abs(relativeXPos - this.dragStartXPos)}px;
-      height: ${Math.abs(relativeYPos - this.dragStartYPos)}px;
+      top: ${isYInverted ? event.pageY : this.dragStartYPos}px;
+      left: ${isXInverted ? event.pageX : this.dragStartXPos}px;
+      width: ${Math.abs(event.pageX - this.dragStartXPos)}px;
+      height: ${Math.abs(event.pageY - this.dragStartYPos)}px;
       opacity: 1;
     `;
 
     // Find selection box bounds (using note-line positions)
-    let yMin = this.dragStartYPos;
-    let yMax = relativeYPos;
-    let xMin = this.dragStartXPos - PAPER_SIDE_MARGIN;
-    let xMax = relativeXPos - PAPER_SIDE_MARGIN;
+    const { x: noteLinesX, y: noteLinesY } = this.noteLinesEl.getBoundingClientRect();
+
+    let yMin = this.dragStartYPos - noteLinesY;
+    let yMax = event.pageY - noteLinesY;
+    let xMin = this.dragStartXPos - noteLinesX;
+    let xMax = event.pageX - noteLinesX;
 
     if (isYInverted) {
       [yMin, yMax] = [yMax, yMin];
@@ -99,8 +93,10 @@ export class Selection extends MBComponent {
       musicBoxStore.setState('appState.isPlaying', false);
     }
 
-    this.dragStartYPos = event.offsetY;
-    this.dragStartXPos = getRelativeXPos(event);
+    // @TODO NEXT: fix the bug where pageY isn't correct when the page is scrolled
+    this.dragStartYPos = event.pageY;
+    this.dragStartXPos = event.pageX;
+
     this.handleDragging(event);
   }
 
@@ -129,6 +125,7 @@ export class Selection extends MBComponent {
   }
 
   deselectAllNotes() {
+    console.log('deselecting all notes');
     let newSelectedNotes = cloneDeep(musicBoxStore.state.appState.selectedNotes);
 
     Object.keys(newSelectedNotes).forEach(pitch => {
@@ -139,10 +136,15 @@ export class Selection extends MBComponent {
   }
 
   render() {
-    this.element.innerHTML = `
-      <div class="selection-drag-zone" title="Select notes"></div>
-      <div class="selection-box"></div>
-    `;
+    // note: we insertAdjacentHTML because it means we don't need to render all the
+    // dynamic child components here (which may simplify initialization). This only
+    // works because this component never re-renders.
+    this.element.insertAdjacentHTML('afterbegin', `
+      <div class="selection">
+        <div class="selection-drag-zone" title="Select notes"></div>
+        <div class="selection-box"></div>
+      </div>
+    `);
 
     this.selectionZoneEl = this.element.querySelector('.selection-drag-zone');
     this.selectionBoxEl = this.element.querySelector('.selection-box');
@@ -151,5 +153,11 @@ export class Selection extends MBComponent {
     this.selectionZoneEl.addEventListener('mousedown', this.handleMouseDown);
     this.selectionZoneEl.addEventListener('mouseup', this.handleMouseUp);
     this.selectionZoneEl.addEventListener('mouseout', this.handleMouseOut);
+
+    this.element.addEventListener('mousedown', (event) => {
+      if (event.currentTarget !== event.target) return;
+
+      this.handleMouseDown(event);
+    });
   }
 }
